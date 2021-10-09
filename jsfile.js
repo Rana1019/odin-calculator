34d50a4a2850a54032392fcabda0a8d917b94cdb
Rana1019 <himanshurana19@gmail.com>
2021-10-10 00:38:03 +0530

    non decimal working calc

--- jsfile.js
+++ jsfile.js
@@ -1,16 +1,17 @@
 function add(x,y)
 {
-    return x+y;
+    
+    return Number(x) + Number(y);
 }
 
 function subtract(x,y)
 {
-    return x-y;
+    return Number(x)-Number(y);
 }
 
 function multiply(x,y)
 {
-    return x*y;
+    return Number(x)*Number(y);
 
 }
 
@@ -18,18 +19,18 @@ function divide(x,y)
 {
     if(y == 0)
     {
-        return "Div by ZERO";
+        return "OOPS";
     }
-    return x/y;
+    return Number(x)/Number(y);
 }
 
 function modulus(x,y)
 {
     if(y == 0)
     {
-        return "Modulus by ZERO";
+        return "OOPS";
     }
-    return x%y;
+    return Number(x)%Number(y);
 }
 
 function operate(x,y, op)
@@ -52,7 +53,7 @@ function operate(x,y, op)
     }
     else if(op == "%")
     {
-        return modulus(x/y);
+        return modulus(x,y);
     }
     
 }
@@ -61,24 +62,100 @@ function operate(x,y, op)
 let temp = "";
 let x1;
 let x2;
+let operation = "";
 const bottom = document.querySelector(".bot"); 
 const display = document.querySelector(".top"); // display div
 console.log(bottom);
+display.textContent = 0;
 
 bottom.addEventListener("click", handleClick);
 
 function handleClick(event)
 {
-console.log(event.target.classList.contains("num"));
+//console.log(event.target.classList.contains("num"));
+
 if(event.target.classList.contains("num"))
-{
-console.log(event.target.id);
-temp += event.target.id;
-console.log()
-console.dir(display);
-display.textContent = temp;
+    {
+    console.log(event.target.id);
+    temp += event.target.id;
+    console.log()
+    console.dir(display);
+    display.textContent = temp;
+    console.log(typeof(temp));
+    }
+   // console.log(event.target.classList.contains("op")+" op treu or false");
+
+    if(event.target.classList.contains("op") &&   temp != "")
+    {
+        if(x1 != "")
+        {
+            
+            x2 = temp;
+            let tempret = operate(x1,x2, operation);
+            operation = event.target.id;
+            console.log("tempret " + tempret);
+            temp = tempret;
+            display.textContent = temp;
+            temp = ""
+            
+            x1 = tempret;
+            x2 = "";
+            
+        }
+        else
+        {
+            x1 = temp;
+            temp = "";
+            operation = event.target.id;
+            console.log(typeof(operation));
+            console.log(event.target.id);
+            console.log(operation+" operation");
+        }
+       
+        
+    }
+
+    if(event.target.classList.contains("equal") &&   operation != "" && temp != "")
+    {
+        console.log(event.target);
+        x2 = temp;
+        
+        temp = "";
+        let ret = operate(x1, x2, operation);
+        console.log(ret + " ret returned");
+       // console.log(typeof(ret)+" ret");
+        if( ret == "OOPS")
+        {
+            return;
+        }
+        temp = ret;
+        x1 = "";
+        x2 = "";
+        display.textContent = temp;
+        operation = "";
+       
+        
+    }
+
+    if(event.target.id == "clear")
+    {
+        temp ="";
+        x1 = "";
+        x2 = "";
+        operation = "";
+        display.textContent = 0;
+    }
+
+    if(event.target.id == "del")
+    {
+        temp = temp.slice(0,-1);
+        display.textContent = temp;
+    }
+
 }
 
 
-}
+
+
+
 
